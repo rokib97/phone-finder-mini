5abc0d011c93980ed4fbcc21bd69ed634d6bc3fe
rokib97 <hasanrokib1997@gmail.com>
2022-03-01 12:11:30 +0600

    handled more error

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+const errorMsg = document.getElementById("error-msg");
+// function to generate a error messeage
+const handleError = (value) => {
+  if (value) {
+    errorMsg.innerHTML = `
+      <marquee behavior="scroll" scrollamount="12" width="75%" direction="left" height="50px">
+      Please input a valid name....
+      </marquee>
+      `;
+  } else {
+    errorMsg.innerHTML = `
+      <marquee behavior="scroll" scrollamount="12" width="75%" direction="left" height="50px">
+      Search phone not found....
+      </marquee>
+      `;
+  }
+};
+
 // function to fetch phone data and find the phone
 const findPhone = () => {
   document.getElementById("card-container").textContent = "";
@@ -7,13 +25,8 @@ const findPhone = () => {
   inputElement.value = "";
 
   // handle error for empty input and number value
-  const errorMsg = document.getElementById("error-msg");
   if (inputValue == "" || !isNaN(inputValue)) {
-    errorMsg.innerHTML = `
-    <marquee width="50%" direction="left" height="50px">
-    Please input valid something....
-    </marquee>
-    `;
+    handleError(true);
     inputElement.value = "";
     document.getElementById("search-result").innerHTML = "";
     document.getElementById("phone-details").innerHTML = "";
@@ -24,12 +37,8 @@ const findPhone = () => {
       .then((res) => res.json())
       .then((data) => {
         // handle error for not found phone data
-        if (data.data.length == 0) {
-          errorMsg.innerHTML = `
-          <marquee width="50%" direction="left" height="50px">
-          Please input valid something....
-          </marquee>
-          `;
+        if (data.data.length === 0) {
+          handleError(false);
           document.getElementById("search-result").innerHTML = "";
           document.getElementById("phone-details").innerHTML = "";
         } else {
@@ -50,7 +59,7 @@ const displayPhone = (phones) => {
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
-        <div class="card h-100 shadow rounded-3 p-3 border-0">
+        <div class="card h-100 shadow-lg rounded-3 p-3 border-0">
           <img src="${phone.image}" class="card-img-top img-fluid w-75 h-75 mx-auto" alt="..." />
             <div class="card-body">
                 <h5 class="card-title text-center text-success">${phone.phone_name}</h5>
@@ -65,8 +74,8 @@ const displayPhone = (phones) => {
     containerDiv.appendChild(div);
     // showing search result brand name
     document.getElementById("search-result").innerHTML = `
-    <h5 class="card-title text-center fw-bolder">Search results for <span class="text-success">"${phone.brand}"</span></h5>
-    <h5 class="card-title text-center fw-bolder">Total items found : <span class="text-success">${phones.length}</span></h5>
+    <h5 class="card-title text-center fw-bolder text-white">Search results for <span class="text-success">"${phone.brand}"</span></h5>
+    <h5 class="card-title text-center fw-bolder text-white">Total items found : <span class="text-success">${phones.length}</span></h5>
     `;
   });
 };
@@ -85,20 +94,36 @@ const displayDetail = (phone) => {
   console.log(phone);
   const detailContainerDiv = document.getElementById("phone-details");
   detailContainerDiv.innerHTML = `
-        <div class="card">
-            <img src="${phone.image}" class="card-img-top w-50 mx-auto p-4" alt="...">
+        <div class="card rounded-3 p-3 border-0 shadow-lg">
+            <img src="${
+              phone.image
+            }" class="card-img-top w-50 mx-auto img-fluid" alt="...">
             <div class="card-body">
                   <h5 class="card-title text-success">${phone.name}</h5>
-                  <p class="card-text">Brand: <span class="text-primary">${phone.brand}</span></p>
-                  <p class="card-text">Release Date: <span class="text-primary">${phone.releaseDate}</span></p>
-                  <p class="card-text text-success">MAIN FEATURES:</span></p>
+                  <p class="card-text">Brand: <span class="text-primary">${
+                    phone.brand
+                  }</span></p>
+                  <p class="card-text">Release Date: <span class="text-primary">${
+                    phone.releaseDate
+                      ? phone.releaseDate
+                      : "No Release Date Found"
+                  }</span></p>
+                  <p class="card-text card-title fw-bold text-success">MAIN FEATURES:</span></p>
                   <ul class="">
-                    <li class=""><span class="text-danger">Chipset</span>: ${phone.mainFeatures.chipSet}</li>
-                    <li class=""><span class="text-danger">Display Size</span>: ${phone.mainFeatures.displaySize}</li>
-                    <li class=""><span class="text-danger">Memory</span>: ${phone.mainFeatures.memory}</li>
-                    <li class=""><span class="text-danger">Storage</span>: ${phone.mainFeatures.storage}</li>
+                    <li class=""><span class="text-danger">Chipset</span>: ${
+                      phone.mainFeatures.chipSet
+                    }</li>
+                    <li class=""><span class="text-danger">Display Size</span>: ${
+                      phone.mainFeatures.displaySize
+                    }</li>
+                    <li class=""><span class="text-danger">Memory</span>: ${
+                      phone.mainFeatures.memory
+                    }</li>
+                    <li class=""><span class="text-danger">Storage</span>: ${
+                      phone.mainFeatures.storage
+                    }</li>
                  </ul>
-                  <p class="card-text text-success">SENSORS:</span></p>
+                  <p class="card-text text-success fw-bold">SENSORS:</span></p>
                   <ul class="">
                     <li class="">${phone.mainFeatures.sensors[0]}</li>
                     <li class="">${phone.mainFeatures.sensors[1]}</li>
@@ -107,14 +132,28 @@ const displayDetail = (phone) => {
                     <li class="">${phone.mainFeatures.sensors[4]}</li>
                     <li class="">${phone.mainFeatures.sensors[5]}</li>
                  </ul>
-                 <p class="card-text text-success">OTHERS:</span></p>
+                 <p class="card-text text-success fw-bold">OTHERS:</span></p>
                   <ul class="">
-                    <li class=""><span class="text-danger">Bluetooth</span>: ${phone.others.Bluetooth}</li>
-                    <li class=""><span class="text-danger">GPS</span>: ${phone.others.GPS}</li>
-                    <li class=""><span class="text-danger">NFC</span>: ${phone.others.NFC}</li>
-                    <li class=""><span class="text-danger">Radio</span>: ${phone.others.Radio}</li>
-                    <li class=""><span class="text-danger">USB</span>: ${phone.others.USB}</li>
-                    <li class=""><span class="text-danger">Wlan</span>: ${phone.others.WLAN}</li>
+                    <li class=""><span class="text-danger">Bluetooth</span>: ${
+                      phone?.others?.Bluetooth
+                        ? phone?.others?.Bluetooth
+                        : "Not Found"
+                    }</li>
+                    <li class=""><span class="text-danger">GPS</span>: ${
+                      phone?.others?.GPS ? phone?.others?.GPS : "Not Found"
+                    }</li>
+                    <li class=""><span class="text-danger">NFC</span>: ${
+                      phone?.others?.NFC ? phone?.others?.NFC : "Not Found"
+                    }</li>
+                    <li class=""><span class="text-danger">Radio</span>: ${
+                      phone?.others?.Radio ? phone?.others?.Radio : "Not Found"
+                    }</li>
+                    <li class=""><span class="text-danger">USB</span>: ${
+                      phone?.others?.USB ? phone?.others?.USB : "Not Found"
+                    }</li>
+                    <li class=""><span class="text-danger">Wlan</span>: ${
+                      phone?.others?.WLAN ? phone?.others?.WLAN : "Not Found"
+                    }</li>
                  </ul>
             </div>
       </div>
